61bdfd9069f415ad7320923cd19687d92fec94dd
sk Lee <znfnfn2140@gmail.com>
2022-11-01 17:48:06 +0900

    react router 데이터

--- react-router2/src/components/Navbar.jsx
+++ react-router2/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div style={{ width: "100%", backgroundColor: "lightgray" }}>
+    <div style={{ width: "100%", backgroundColor: "lightgray", padding: "10px" }}>
       <h1>Navbar 입니다</h1>
+      <NavLink to="/">Home</NavLink>
+      <span> | </span>
+      <NavLink to="/writeform">Write</NavLink>
+      <span> | </span>
+      <NavLink to="/boardlist">Board</NavLink>
     </div>
   );
 };
--- react-router2/src/pages/Writeform.jsx
+++ react-router2/src/pages/Writeform.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { StateContext, SetContext } from "../context/ContextComp";
 
 const Writeform = () => {
+  const boarlist = useContext(StateContext);
+  const setBoard = useContext(SetContext);
+  const [context, setContext] = useState("");
+
+  // 데이터와 관련된 내용은 서버측에서 관리하는 내용
+  // 서버를 사용하고 있지 않기 때문에, 전체 데이터를 리액트에서 관리
+  const addBoard = () => {
+    const newList = boarlist.concat({  // 게시물 데이터 추가
+      id: boarlist.length + 1,         // 게시물 id
+      context: context,                // 게시물 내용
+    });
+    setBoard(newList);
+  };
+
   return (
     <div>
       <h1>Wirteform 입니다</h1>
+      <textarea
+        cols="70" rows="20"
+        onChange={(e) => {setContext(e.target.value);}}
+      ></textarea>
+      <p>{context}</p>
+      <button onClick={addBoard}>발행</button>
     </div>
   );
 };
