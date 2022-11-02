7b8b3fdd044be95bb70424c6ccf5ed940e748916
sk Lee <znfnfn2140@gmail.com>
2022-11-02 11:37:46 +0900

    react router ajax 추가

--- react-router2/src/App.js
+++ react-router2/src/App.js
@@ -3,13 +3,26 @@ import { StateContext, SetContext } from "./context/ContextComp";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Writeform from "./pages/Writeform";
 import BoardList from "./pages/BoardList";
 import BoardContext from "./pages/BoardContext";
 
 function App() {
   const [boardlist, setBoardlist] = useState([]);
+
+  // 실행하자마자 ajax를 통해 값을 들고올 수 있다.
+  useEffect(() => {
+    // Fetch를 사용한 ajax
+    async function getData() {
+      // reponse로 값을 받아오는 시간이 걸림
+      const response = await fetch("boardlistdata.json");
+      const data = await response.json();
+      console.log(data);
+      setBoardlist(data.boardlist);
+    }
+    getData();
+  }, []);
 
   return (
     <div className="App">
@@ -21,7 +34,7 @@ function App() {
               {/* path='/'대신 index 사용 */}
               {/* index : 해당 루트 내에서 가장 첫 페이지를 의미 */}
               <Route index element={<Home />} />
-              <Route path="writeform" element={<Writeform />}/>
+              <Route path="writeform" element={<Writeform />} />
               <Route path="boardlist" element={<BoardList />}>
                 <Route path=":id" element={<BoardContext />} />
               </Route>
